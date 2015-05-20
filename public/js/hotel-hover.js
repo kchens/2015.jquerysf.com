9b08259b1e8c77083957eed73a61f3020ff7a300
kchens <kkch3n@gmail.com>
2015-05-20 13:37:13 -0700

    adds working hover color

--- public/js/hotel-hover.js
+++ public/js/hotel-hover.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-  console.log("YOLO");
+    if (Modernizr.touch) {
+      // show the close overlay button
+      $(".close-overlay").removeClass("hidden");
+      // handle the adding of hover class when clicked
+      $(".img").click(function(e){
+          if (!$(this).hasClass("hover")) {
+              $(this).addClass("hover");
+          }
+      });
+      // handle the closing of the overlay
+      $(".close-overlay").click(function(e){
+          e.preventDefault();
+          e.stopPropagation();
+          if ($(this).closest(".img").hasClass("hover")) {
+              $(this).closest(".img").removeClass("hover");
+          }
+      });
+  } else {
+    // handle the mouseenter functionality
+    $(".hotel-column").mouseenter(function(){
+        $(this).addClass("hover");
+    })
+    // handle the mouseleave functionality
+    .mouseleave(function(){
+        $(this).removeClass("hover");
+    });
+  }
 });
