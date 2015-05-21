62d3c9eb7d70e28e8f5d3decf8f98eb47b3fef81
kchens <kkch3n@gmail.com>
2015-05-21 10:42:44 -0700

    adds most of the finalized hotel info

--- public/js/hotel-hover.js
+++ public/js/hotel-hover.js
@@ -4,21 +4,21 @@ $(document).ready(function(){
       // $(".close-overlay").removeClass("hidden");
 
       // handle the adding of hover class when clicked
-      $(".hotel").click(function(e){
-          if (!$(this).hasClass("hover")) {
-              $(this).addClass("hover");
-              $(this).find(".close-overlay").removeClass("hidden");
-          }
-      });
-      // handle the closing of the overlay
-      $(".close-overlay").click(function(e){
-          e.preventDefault();
-          e.stopPropagation();
-          if ($(this).closest(".hotel").hasClass("hover")) {
-              $(this).closest(".hotel").removeClass("hover");
-              $(this).addClass("hidden");
-          }
-      });
+      // $(".hotel").click(function(e){
+      //     if (!$(this).hasClass("hover")) {
+      //         $(this).addClass("hover");
+      //         $(this).find(".close-overlay").removeClass("hidden");
+      //     }
+      // });
+      // // handle the closing of the overlay
+      // $(".close-overlay").click(function(e){
+      //     e.preventDefault();
+      //     e.stopPropagation();
+      //     if ($(this).closest(".hotel").hasClass("hover")) {
+      //         $(this).closest(".hotel").removeClass("hover");
+      //         $(this).addClass("hidden");
+      //     }
+      // });
   } else {
     // handle the mouseenter functionality
     $(".hotel").mouseenter(function(){
